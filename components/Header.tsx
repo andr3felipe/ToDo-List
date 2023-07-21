0069a8bdc76da832f4f291aaf194708d5ef864da
andr3felipe <andre.felipe_680@hotmail.com>
2023-07-21 18:04:58 -0300

    add dayjs

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import dayjs from 'dayjs'
 
 export default function Header() {
+  const day = dayjs().format('dddd')
+  const fullDate = dayjs().format('DD MMMM YYYY')
+
   return (
     <header className="flex justify-between">
       <div>
-      <h3 className="text-primary text-lg font-bold">Thursday</h3>
-      <p className="text-xs text-date">09 June 2023</p>
+      <h3 className="text-primary text-lg font-bold">{day}</h3>
+      <p className="text-xs text-date">{fullDate}</p>
       </div>
       <div>
         <Image src="/profile.png" width={50} height={50} alt="" />
